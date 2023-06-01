0787a2b8bb4981d8020f36849aaedcd67356a6fb
MichaelBinz <134392768+MichaelBinz@users.noreply.github.com>
2023-06-01 18:35:17 +0200

    1 file modified

--- index.tsx
+++ index.tsx
@@ -6,15 +6,21 @@ import Controls from './Controls';
 
 import './style.css';
 
+// should be const
 var initialTime = 60;
 
 const App: React.FC = () => {
   const [time, setTime] = useState(initialTime);
+  //use intervalReference
   const [timer, setTimer] = useState<number | null>(null);
 
   const startTimer = () => {
+    //maybe improve (so it is not too deep)
+    // return after if statement, keeps it less deep
     if (!timer) {
       setTimer(
+        // this is not trustworthy for "actual time", should be changed to use date functions
+        // request animation framefor proper performance (might not make sense in this case)
         setInterval(() => {
           setTime((time) => time - 1);
         }, 1000)
